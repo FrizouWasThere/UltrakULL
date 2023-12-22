024bbcc1de730e40d56868f6ce6caff3e8161ed0
FrizouWasThere <101941926+FrizouWasThere@users.noreply.github.com>
2023-12-22 13:54:37 -0500

    Update StyleBonusStrings.cs
    Added (hopefully) all new style bonuses in this update

--- UltrakULL/StyleBonusStrings.cs
+++ UltrakULL/StyleBonusStrings.cs
@@ -54,6 +54,7 @@ namespace UltrakULL
                 case "ultrakill.instakill": { return "<color=green>" + LanguageManager.CurrentLanguage.style.style_instakill + "</color>"; }
                 case "ultrakill.interruption": { return "<color=green>" + LanguageManager.CurrentLanguage.style.style_interruption + "</color>"; }
                 case "ultrakill.kill": { return LanguageManager.CurrentLanguage.style.style_kill; }
+                case "ultrakill.landyours": { return "<color=green>" + LanguageManager.CurrentLanguage.style.style_landyours + "</color>"; }
                 case "ultrakill.limbhit": { return LanguageManager.CurrentLanguage.style.style_limbshot; }
                 case "ultrakill.mauriced": { return LanguageManager.CurrentLanguage.style.style_mauriced; }
                 case "ultrakill.multikill": { return "<color=orange>" + LanguageManager.CurrentLanguage.style.style_multikill + "</color>"; }
@@ -64,11 +65,20 @@ namespace UltrakULL
                 case "ultrakill.projectileboost": { return "<color=green>" + LanguageManager.CurrentLanguage.style.style_projectileboost + "</color>"; }
                 case "ultrakill.quickdraw": { return "<color=#11dfef>" + LanguageManager.CurrentLanguage.style.style_quickdraw + "</color>"; }
                 case "ultrakill.ricoshot": { return "<color=#11dfef>" + LanguageManager.CurrentLanguage.style.style_ricoshot + "</color>"; }
+                case "ultrakill.rocketreturn": { return "<color=#00ffffff>" + LanguageManager.CurrentLanguage.style.style_rocketreturn + "</color>"; }
+                case "ultrakill.roundtrip": { return "<color=green>" + LanguageManager.CurrentLanguage.style.style_roundtrip + "</color>"; }
                 case "ultrakill.secret": { return "<color=#11dfef>" + LanguageManager.CurrentLanguage.style.style_secret + "</color>"; }
+                case "ultrakill.serve": { return "<color=#00ffffff>" + LanguageManager.CurrentLanguage.style.style_served + "</color>"; }
                 case "ultrakill.splattered": { return LanguageManager.CurrentLanguage.style.style_splattered; }
+                case "ultrakill.strike": { return "<color=#00ffffff>" + LanguageManager.CurrentLanguage.style.style_strike + "</color>"; }
                 case "ultrakill.triplekill": { return "<color=orange>" + LanguageManager.CurrentLanguage.style.style_triplekill + "</color>"; }
 
-                default: return "";
+                case "ultrakill.shotgunhit": { return ""; }
+                case "ultrakill.nailhit": { return ""; }
+                case "ultrakill.explosionhit": { return ""; }
+                case "ultrakill.firehit": { return ""; }
+                case "": { return ""; }
+                default: { Logging.Warn("Missing style translation: " + inputBonus); return inputBonus; }
             }
         }
 
@@ -79,18 +89,27 @@ namespace UltrakULL
             //Try and keep this alphabetical as it gets bigger over time.
                 switch (regexinput)
                 {
+                    case "BOILED": { return LanguageManager.CurrentLanguage.style.style_boiled; }
                     case "CONDUCTOR": { return LanguageManager.CurrentLanguage.style.style_conductor; }
                     case "CRUSHED": { return LanguageManager.CurrentLanguage.style.style_crushed; } 
                     case "ENVIROKILL": { return LanguageManager.CurrentLanguage.style.style_envirokill; }
                     case "FALL": { return LanguageManager.CurrentLanguage.style.style_fall; }
                     case "FRIED": { return LanguageManager.CurrentLanguage.style.style_fried; }
+                    case "FOR THEE": { return LanguageManager.CurrentLanguage.style.style_forthee; }
+                    case "GUARD BREAK": { return "<color=green>" + LanguageManager.CurrentLanguage.style.style_guardbreak + "</color>"; }
+                    case "LONG WAY DOWN": { return LanguageManager.CurrentLanguage.style.style_longwaydown; }
+                    case "LOST": { return LanguageManager.CurrentLanguage.style.style_lost; }
+                    case "M.A.D.": { return LanguageManager.CurrentLanguage.style.style_mad; }
                     case "MINCED": { return LanguageManager.CurrentLanguage.style.style_minced; }
                     case "OUT OF BOUNDS": { return LanguageManager.CurrentLanguage.style.style_outofbounds; }
+                    case "PANCAKED": { return LanguageManager.CurrentLanguage.style.style_pancaked; }
                     case "RICOSHOT": { return LanguageManager.CurrentLanguage.style.style_ricoshot; }
+                    case "ROADKILL": { return LanguageManager.CurrentLanguage.style.style_roadkill; }
                     case "SCRONGLED": { return LanguageManager.CurrentLanguage.style.style_scrongled; }
                     case "SCRONGBONGLED": { return LanguageManager.CurrentLanguage.style.style_scrongbongled; }
                     case "SCRINDONGULODED": { return LanguageManager.CurrentLanguage.style.style_scrindonguloded; }
                     case "SHREDDED": { return LanguageManager.CurrentLanguage.style.style_shredded; }
+                    case "TRAMPLED": { return LanguageManager.CurrentLanguage.style.style_trampled; }
                     case "ZAPPED": { return LanguageManager.CurrentLanguage.style.style_zapped; }
                     case "why are you even spawning enemies here": { return LanguageManager.CurrentLanguage.style.style_why; }
                     case "": { return ""; }
